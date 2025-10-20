1cc820f9f1572e804db94ac4e87ad36adeab6ea2
User1 <nikolayrutin@gmail.com>
2025-10-20 22:30:35 +0500

    выполнено задание 5 - деление на модули

--- js/functions.js
+++ js/functions.js
@@ -9,7 +9,19 @@ const isPalindrome = (input) => {
   return normalized === reversed;
 };
 
+const timeToMinutes = (timeStr) => {
+  const [hours, minutes] = timeStr.split(':').map(Number);
+  return hours * 60 + minutes;
+};
+
+const isMeetingWithinWorkday = (workStart, workEnd, meetingStart, meetingDuration) => {
+  const meetingStartMinutes = timeToMinutes(meetingStart);
+  const meetingEndMinutes = meetingStartMinutes + meetingDuration;
+  return meetingStartMinutes >= timeToMinutes(workStart) && meetingEndMinutes <= timeToMinutes(workEnd);
+};
+
+//console.log(isMeetingWithinWorkday('08:00', '14:30', '14:00', 90));
 //console.log(checkLength('Hello', 5));
 //console.log(isPalindrome('Лёша на полке клопа нашёл '));
-export {checkLength, isPalindrome};
+export {checkLength, isPalindrome, isMeetingWithinWorkday};
 
